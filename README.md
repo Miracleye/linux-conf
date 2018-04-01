756806caaab31fee2f792d410702741e1d470cf7
miracleye <zhou_software@outlook.com>
2018-04-01 19:27:11 +0800

    add *

---
+++ README.md
@@ -0,0 +1,3 @@
+1. A simple .vimrc for vim -- **Usage: cp .vimrc ~**
+2. vim plugins conf -- **Usage: cp -r .vim ~**
+3. Source code pro font in the .font dir -- **Usage: cp -r .fonts ~/**
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# install some nessessary packages
+apt-get install vim
+apt-get install ctags
+apt-get install indent
+apt-get install vim-scripts
+apt-get install build-essential
+apt-get install cmake
+apt-get install python3
+apt-get install python-dev
+
+# Change home dirs' name to en
+# export LANG=en_US
+# xdg-user-dirs-gtk-update
+# export LANG=zh_CN.UTF-8
+
+# Configure vim
+# cd ~
+# wget -qO- https://raw.github.com/ma6174/vim/master/setup.sh | sh -x
